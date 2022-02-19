6a0369326eee2e54c3cee0f5270eb76642e7ff03
Kristina <kristina.diamond@kryoinc.com>
2022-02-19 17:36:41 -0500

    set up vue project and interact.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" class="draggable" src="./assets/logo.png">
+    <div>RASPBERRY POCKETS</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import interact from 'interact.js';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data(){
+    return {}
+  },
+  mounted() {
+    interact('.draggable').draggable({
+      onmove: this.dragMoveListener,
+      inertia: false,
+    });
+  },
+  methods: {
+    dragMoveListener(event) {
+      var target = event.target,
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      // translate the element's CSS/Styling (so that it can move starting from the line above data positioning)
+      target.style.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
+      target.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate('+ '0' + 'deg)';
+      // update the posiion attributes so that it keeps moving
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    }
   }
 }
 </script>
