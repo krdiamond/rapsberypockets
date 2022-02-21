08a309c8e938002d3d9f492d2fc4eb17a0afc89e
Kristina <kristina.diamond@kryoinc.com>
2022-02-21 15:03:09 -0500

    some products snap and some products dontt

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
-    <img class="draggable" :src="require('./assets/cool-drip.png')"/> 
-    <img class="draggable" :src="require('./assets/pink-drip.png')"/> 
     <div>RASPBERRY POCKETS</div>
+
+    <div class="grid-container">
+      <div id="1" class="grid-item">1</div>
+      <div id="2" class="grid-item">2</div>
+      <div id="3" class="grid-item">3</div>  
+      <div id="4" class="grid-item">4</div>
+      <div id="5" class="grid-item">5</div>
+      <div id="6" class="grid-item">6</div>  
+      <div id="7" class="grid-item">7</div>
+      <div id="8" class="grid-item">8</div>
+      <div id="9" class="grid-item">9</div>  
+    </div>
+
+    <div>
+      <img id="drip_1" class="drip draggableWithSnap" dataSnap="2" :src="require('./assets/cool-drip.png')"/> 
+    </div>
+    <div>
+      <img id="drip_2" class="drip draggable" dataSnap="6" :src="require('./assets/pink-drip.png')"/>
+    </div>
+      
+    
+
   </div>
 </template>
 
 <script>
+
 import interact from 'interact.js';
 
 export default {
@@ -17,6 +38,16 @@ export default {
   },
   mounted() {
     const app = document.getElementById('app');
+    interact('.draggableWithSnap').draggable({
+      onmove: this.dragMoveListener,
+      onstart: this.findSnapLocation,
+      inertia: false,
+      restrict: {
+        restriction: app,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1},
+        endOnly: false
+      },
+    });
     interact('.draggable').draggable({
       onmove: this.dragMoveListener,
       inertia: false,
@@ -28,10 +59,33 @@ export default {
     });
   },
   methods: {
+    findSnapLocation(event) {
+      console.log(event);
+      const snapLocation = document.getElementById(event.target.getAttribute('dataSnap'));
+      let snapLocationRectangle = snapLocation.getBoundingClientRect();
+      let snapPostion = {
+        x: snapLocationRectangle.left + snapLocationRectangle.width  / 2,
+        y: snapLocationRectangle.top  + snapLocationRectangle.height / 2,
+      }
+
+      let homeLocationRectangle = event.target.parentElement.getBoundingClientRect();
+      let homePosition = {
+        x: homeLocationRectangle.left + homeLocationRectangle.width  / 2,
+        y: homeLocationRectangle.top  + homeLocationRectangle.height / 2
+      }
+
+      event.interactable.draggable({
+        snap: {
+          targets: [snapPostion, homePosition],
+          relativePoints: [ { x: 0.5, y: 0.5 } ],
+          endOnly: true
+        }
+      })
+    },
     dragMoveListener(event) {
       var target = event.target,
-          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
       // translate the element's CSS/Styling (so that it can move starting from the line above data positioning)
       target.style.webkitTransform = 'translate(' + x + 'px, ' + y + 'px)';
       target.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate('+ '0' + 'deg)';
@@ -43,22 +97,9 @@ export default {
 }
 </script>
 
-<style>
-body {
-  margin: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  height: 99vh;
-  touch-action: none;
-  overflow: hidden;
-  border: 3px solid black;
-}
-img {
-  width: 300px;
-}
-</style>
+
+
+
+
+
+
